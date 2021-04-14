e013bae5c91e337e2317abe42b2651a94a8a2f74
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-04-14 15:55:14 -0700

    TDD createUser, function passes test

--- local-storage-utils.js
+++ local-storage-utils.js
@@ -1,22 +1,27 @@
 const USER = 'USER';
 
 export function getUser(){
-    
+    // get user from local storage
+    // parse string
+    // return parsed user
 }
 
-export function saveUser() {
-
+export function saveUser(account) {
+    // stringify account
+    const stringAccount = JSON.stringify(account);
+    // set to local storage
+    localStorage.setItem(USER, stringAccount);
 }
 
 export function createUser(username, password) {
     // check if username already exists
     // create username from user input
-    const object = {
+    const account = {
         username,
         password,
         todo: []
     };
-    return object;
+    return account;
 }
 
 export function checkPassword() {
