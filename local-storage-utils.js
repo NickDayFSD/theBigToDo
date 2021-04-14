9d45aa29d2066d9adf423748e9f4130f766409d6
Joseph Davis <joethebartenderpdx@gmail.com>
2021-04-14 14:37:56 -0700

    wrote createUser test

---
+++ local-storage-utils.js
@@ -0,0 +1,22 @@
+const USER = 'USER';
+
+export function getUser(){
+    
+}
+
+export function saveUser() {
+
+}
+
+export function createUser(user, password) {
+    // check if username already exists
+    
+    // create username from user input
+    // create password from user input
+    //  create empty 'todo' array
+    // store ^^^ into an object
+}
+
+export function checkPassword() {
+
+}
--- test/example.test.js
+++ test/example.test.js
@@ -1,9 +1,55 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
-const test = QUnit.test;
+const { test, skip } = QUnit.test;
 
 test('time to test a function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = {
+        username: 'phil',
+        password: '666',
+        todos= []
+    }
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = true;
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+skip('time to test a function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = true;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = true;
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+skip('time to test a function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = true;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = true;
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+skip('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
